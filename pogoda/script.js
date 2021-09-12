039c855be32712950763b4ad1d97a28f81e8d11d
MagdalenaKumala <79920505+MagdalenaKumala@users.noreply.github.com>
2021-09-12 21:21:21 +0200

    Update script.js

--- pogoda/script.js
+++ pogoda/script.js
@@ -1,16 +1,14 @@
 let weather = {
-  apiKey: "API KEY GOES HERE",
+  apiKey: "6de0841d48aec019f4175cfbb7a7c0f8",
   fetchWeather: function (city) {
     fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric&appid=" +
+      "https://api.openweathermap.org/data/2.5/weather?q=Krakow&appid=6de0841d48aec019f4175cfbb7a7c0f8" 
         this.apiKey
     )
       .then((response) => {
         if (!response.ok) {
-          alert("No weather found.");
-          throw new Error("No weather found.");
+          alert("Nie znalezniono prognozy pogody.");
+          throw new Error("Nie znaleziono prognozy pogody.");
         }
         return response.json();
       })
@@ -21,7 +19,7 @@ let weather = {
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".city").innerText = "Prognoza pogody dla " + name;
     document.querySelector(".icon").src =
       "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".description").innerText = description;
